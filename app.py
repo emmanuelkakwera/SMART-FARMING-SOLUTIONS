9d1cb44db44b71df2b5636c88c7cb2d84a9a30bf
Emmanuel kakwera kakwera <emmanuelkakwera4@gmail.com>
2025-11-12 10:18:20 +0200

    Adding smart farming solution project

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/soil')
+def soil():
+    return render_template('soil.html')
+
+@app.route('/animals')
+def animals():
+    return render_template('animals.html')
+
+@app.route('/animals/<category>')
+def animal_category(category):
+    return render_template('animal_category.html', category=category)
+
+if __name__ == '__main__':
+    app.run(debug=True)
