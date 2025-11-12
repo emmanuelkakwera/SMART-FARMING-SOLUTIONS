0861303104ed789262a1a8716a54b22937b2d128
Emmanuel kakwera kakwera <emmanuelkakwera4@gmail.com>
2025-11-12 16:53:52 +0200

    Added gunicorn

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def animal_category(category):
     return render_template('animal_category.html', category=category)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
